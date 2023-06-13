bf08914a55202da2e4166eac1d5e33506adad57f
Shane <36411961+ShaneCDev@users.noreply.github.com>
2023-06-13 15:48:03 +0100

    added slug field to products, updated images from jpeg to webp for performance, updated nav links and btt btn styling

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -27,6 +28,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    slug = models.SlugField(max_length=256, unique=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
